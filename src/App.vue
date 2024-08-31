25103929c023be5e060d16eca1321e24c317c09c
Sonali-Rawat <programmergithub789@gmail.com>
2024-08-31 20:42:31 +0530

    Adding main files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-100 min-h-screen p-8">
+    <h1 class="text-4xl font-bold mb-6 text-center">Clothing Store</h1>
+    <div class="mb-6 flex justify-center">
+      <input 
+        v-model="query" 
+        @input="search" 
+        type="text" 
+        placeholder="Search for clothing..." 
+        class="w-full p-4 pl-12 border border-gray-300 rounded-lg shadow-md bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
+           />
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div 
+        v-for="item in filteredItems" 
+        :key="item.id" 
+        class="bg-white p-4 rounded-lg shadow-md"
+      >
+        <img 
+          :src="item.image" 
+          alt="Item image" 
+          class="w-full h-48 object-cover rounded-lg mb-2"
+        />
+        <h2 class="text-xl font-semibold">{{ item.name }}</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      query: '',
+      items: [
+        { id: 1, name: 'T-Shirt', image: 'https://www.creativefabrica.com/wp-content/uploads/2021/10/02/Pixel-Art-TShirt-Graphics-18210223-1-1-580x387.jpg' },
+        { id: 2, name: 'Jeans', image: 'https://www.creativefabrica.com/wp-content/uploads/2021/10/02/Pixel-Art-TShirt-Graphics-18210223-1-1-580x387.jpg' },
+        { id: 3, name: 'Jacket', image: 'https://www.creativefabrica.com/wp-content/uploads/2021/10/02/Pixel-Art-TShirt-Graphics-18210223-1-1-580x387.jpg' }
+        // Add more items as needed
+      ],
+      filteredItems: []
+    };
+  },
+  methods: {
+    search() {
+      if (this.query.length > 3) {
+        this.filteredItems = this.items.filter(item => 
+          item.name.toLowerCase().includes(this.query.toLowerCase())
+        );
+      } else {
+        this.filteredItems = [];
+      }
+    }
+  },
+  mounted() {
+    this.filteredItems = this.items;
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Optional: Add custom styles here */
 </style>
